63a61b98e8c5a7f94c830b4cf9c0b70513da2542
Reeshav <reeshavmaharjan9@gmail.com>
2026-08-21 10:50:17 +0545

    Added Search Feature

--- src/data/Products.js
+++ src/data/Products.js
@@ -1,24 +1,24 @@
 const products = [
   {
-    name: "Dhakapa Bag1",
+    name: "Dhakapa BlueBag",
     category: "BagPack",
     price: "Rs 2000",
-    image: "https://scontent.fktm8-1.fna.fbcdn.net/v/t39.30808-6/506400061_122104842488900728_8340199969717426730_n.jpg?stp=dst-jpg_tt6&cstp=mx1080x1080&ctp=s1080x1080&_nc_cat=100&ccb=1-7&_nc_sid=833d8c&_nc_ohc=C1j7rta1G2wQ7kNvwEBMZRe&_nc_oc=AdoZvmqE7BASnn4QzUqFroqe8Wzq_mg_MxqHRZ7VzprOtzjudIV1n5-LM2HCQhxQfHt2RcweIRUY6CnYvTFb0k0m&_nc_zt=23&_nc_ht=scontent.fktm8-1.fna&_nc_gid=Mc5y2Sn5HSFVEDBke4Sdhw&_nc_ss=7b2a8&oh=00_AQG2HgVBPaoP2Rf0K42XnXMEEpZ7bqHU0WECZ_kl3r6XOA&oe=6A84A3FE"
+    image: "https://i.ibb.co/q3jkS6G3/Blue-Bag.jpg"
   },
   {
-    name: "Dhakapa Bag2 ",
+    name: "Dhakapa BlackBag ",
     category: "BagPack",
     price: "Rs 2000",
-    image: "https://scontent.fktm8-1.fna.fbcdn.net/v/t39.30808-6/506353167_122104842452900728_3436334335962449908_n.jpg?stp=dst-jpg_tt6&cstp=mx1080x1080&ctp=s1080x1080&_nc_cat=109&ccb=1-7&_nc_sid=833d8c&_nc_ohc=mXMIAIEp7agQ7kNvwEToK34&_nc_oc=Adq7AEVUoUrv3kibM3nxqbmOo0DHAu7JD4KzG84x4Pk7jo9TrxOVGeUhvS26najR_-mkFQ2x93XyWgZHcPqEg_ON&_nc_zt=23&_nc_ht=scontent.fktm8-1.fna&_nc_gid=z0gQf6wMS6MGCGkBEa1jvQ&_nc_ss=7b2a8&oh=00_AQFUlQqb1fRWaHYxnRqTthl-YLt0XFxy8XlIefutOtcImw&oe=6A84A9E7"
+    image: "https://i.ibb.co/PswvygbC/Black-Bag.jpg"
   },
   {
-    name: "Dhakap bag3",
+    name: "Dhakap RedBag",
     category: "BagPack",
     price: "Rs 2000",
-    image: "https://scontent.fktm8-1.fna.fbcdn.net/v/t51.75761-15/515121366_17855600775460913_6493380573252171005_n.jpg?stp=dst-jpg_tt6&cstp=mx1080x1080&ctp=s1080x1080&_nc_cat=108&ccb=1-7&_nc_sid=127cfc&_nc_ohc=w3UcU-8ia5sQ7kNvwFdfhrT&_nc_oc=AdqRdPtVx9oXQk0cAPbnvjCJ05upct4zrRubHBkKMOKQ2QA16prTWkpEa0G2l2vHyp8RYDnbAU9iflikcrbMI4UK&_nc_zt=23&_nc_ht=scontent.fktm8-1.fna&_nc_gid=JoOff9P36SCnQa-Zs9C9lA&_nc_ss=7b2a8&oh=00_AQEcX64oL-b3PH--A3PRy_FTlzyprkK1qEcFNX2_bgFW-Q&oe=6A84AE6E"
+    image: "https://i.ibb.co/JjR4749t/Red-Bag.jpg"
   },
-   {
-    name: " Tavel Dhakapa bag",
+  {
+    name: "Travel  bag",
     category: "TravelBag",
     price: "Rs 2600",
     image: "https://i.ibb.co/nNrcPYtw/Travel-Bag.png"
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React,{useState} from "react";
 import products from "../data/Products";
 import ProductsCard from "../components/ProductsCard";
-import SearchBar from "../components/SearchBar";
+import { CiSearch } from "react-icons/ci";
+
 
 const ProductsPage = () => {
+   const [input,setInput] = useState('');
+
+    const handleUserInput =(e)=>{
+        setInput(e.target.value);  
+    }
   return (
     <>
       <div className="max-w-7xl m-auto px-6 py-4">
@@ -42,11 +48,38 @@ const ProductsPage = () => {
           </div>
         </section>
 
-        <SearchBar />
+           <main>
+          {/* Toolbar */}
+          <div className="border max-w-7xl m-auto py-3  rounded-2xl w-full mt-6 flex justify-between">
+            <div className="flex items-center ml-5 flex-1 border-none">
+              <CiSearch size={25} />
+              <input
+                type="text"
+                value={input}
+                placeholder="Search products..."
+                className="ml-4 font-serif w-full outline-none "
+                onChange={handleUserInput}
+              />
+            </div>
+            <div className="h-10 border-l border-gray-300 mx-4"></div>
+
+            <div className="flex gap-2 mr-4 font-serif text-center ">
+              <button className="border w-23  py-2 rounded-xl flex justify-center  ">Filter</button>
+              <select className="border py-2 rounded-xl text-center">
+                <option>Sort by: Featured</option>
+                <option>Price: Low to High</option>
+                <option>Price: High to Low</option>
+                <option>Newest</option>
+              </select>
+            </div>
+          </div>
+        </main>
         {/* Products Section */}
 
         <div className="flex gap-10 justify-center mt-10 ">
-          {products.map((products) => {
+          {products.filter((item)=>{
+            return input.toLowerCase() === ''? item : item.name.toLowerCase().includes(input.toLowerCase())
+          }).map((products) => {
             return <ProductsCard props={products} />;
           })}
         </div>
